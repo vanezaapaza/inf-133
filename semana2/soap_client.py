f6e3130c367c9b5ee24ea127e13d8e4f188d6de2
vaneza apaza <vanezaapazahinojosa@gmail.com>
2024-03-01 03:59:05 -0400

    tarea

--- semana2/soap_client.py
+++ semana2/soap_client.py
@@ -1,6 +1,6 @@
 from zeep import Client
 
-client = Client('http://localhost:8000/')
+client = Client('http://localhost:8000')
 
-result = client.service.Saludar(nombre="Tatiana")
+result = client.service.Saludar(nombre="Vaneza")
 print(result)
